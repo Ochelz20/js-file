45a1deb1e3cd3456eb6ce2c5209d443686cff204
ochelz <ochelivictor@gmail.com>
2025-05-13 10:28:52 +0100

    java script training

---
+++ index.js
@@ -0,0 +1,36 @@
+var y =10;
+
+let username = prompt('Enter your name')
+console.log(username);
+document.getElementById('name').innerHTML = username
+
+
+let school = prompt('Enter your school name')
+console.log(school);
+document.getElementById('school').innerHTML = school
+
+document.getElementById('message').innerHTML += 'Hello world<br>'
+
+let address = prompt('Enter your house address')
+console.log(address)
+document.getElementById('location').innerHTML = address
+
+let sex = prompt('Enter your gender')
+console.log(sex)
+document.getElementById('gender').innerHTML = sex
+
+// concatenation
+console.log('my name is ' + username + 'I attend' + school );
+console.log('my name is ' , username , 'I attend' , school );
+
+// template literal `${}`
+console.log(`my name is ${username}. I attend ${school}`);
+document.getElementById('message').innerHTML += `my name is ${username}. I attend ${school}`
+
+// operators
+// arithmetic + - * / % ** ++ --
+// assignment = += -= *= /= %= **= ++ -- -=
+// logical && || !
+// comparison == === != !== >= <=
+// ternary ? :
+
