5dd6b51ef0c05a13943b3a715373b7e306f241f0
ochelz <ochelivictor@gmail.com>
2025-05-14 08:42:40 +0100

    training on javascript

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var y =10;
+// var y =10;
 
-let username = prompt('Enter your name')
-console.log(username);
-document.getElementById('name').innerHTML = username
+// let username = prompt('Enter your name')
+// console.log(username);
+// document.getElementById('name').innerHTML = username
 
 
-let school = prompt('Enter your school name')
-console.log(school);
-document.getElementById('school').innerHTML = school
+// let school = prompt('Enter your school name')
+// console.log(school);
+// document.getElementById('school').innerHTML = school
 
-document.getElementById('message').innerHTML += 'Hello world<br>'
+// document.getElementById('message').innerHTML += 'Hello world<br>'
 
-let address = prompt('Enter your house address')
-console.log(address)
-document.getElementById('location').innerHTML = address
+// let address = prompt('Enter your house address')
+// console.log(address)
+// document.getElementById('location').innerHTML = address
 
-let sex = prompt('Enter your gender')
-console.log(sex)
-document.getElementById('gender').innerHTML = sex
+// let sex = prompt('Enter your gender')
+// console.log(sex)
+// document.getElementById('gender').innerHTML = sex
 
-// concatenation
-console.log('my name is ' + username + 'I attend' + school );
-console.log('my name is ' , username , 'I attend' , school );
+// // concatenation
+// console.log('my name is ' + username + 'I attend' + school );  
+// console.log('my name is ' , username , 'I attend' , school );
 
-// template literal `${}`
-console.log(`my name is ${username}. I attend ${school}`);
-document.getElementById('message').innerHTML += `my name is ${username}. I attend ${school}`
+// // template literal `${}`
+// console.log(`my name is ${username}. I attend ${school}`);
+// document.getElementById('message').innerHTML += `my name is ${username}. I attend ${school}`
 
 // operators
 // arithmetic + - * / % ** ++ --
@@ -34,3 +34,19 @@ document.getElementById('message').innerHTML += `my name is ${username}. I atten
 // comparison == === != !== >= <=
 // ternary ? :
 
+
+// Assignment
+
+let firstNum = prompt('Enter the first number')
+console.log(firstNum)
+
+let secondNum = prompt('Enter the second number')
+console.log(secondNum)
+
+console.log(firstNum + secondNum)
+console.log(firstNum - secondNum)
+console.log(firstNum * secondNum)
+console.log(firstNum / secondNum)
+console.log(firstNum % secondNum)
+console.log(firstNum ** secondNum)
+console.log(firstNum++ )
